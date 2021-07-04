e81fb877279f2af2726cb83c8dea9d3cf2b68937
hakan bahşiş <hakan.bahsis@hotmail.com>
2021-07-04 23:08:00 +0300

    04.07.2021

---
+++ OOP1/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP1
+{
+   public class Product 
+    {
+        public int Id { get; set; }
+        public int CategoryId { get; set; }//KategoryId
+        public string ProductName { get; set; }//Adı
+        public double UnitPrice { get; set; }//Birim Fiyatı
+        public int UnitsInStock { get; set; }//Stok Miktarı
+
+        //CRUD -- Creat - Read -- Updated - Delete
+    }
+}
---
+++ OOP1/ProductManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP1
+{
+    class ProductManager //Ürün ile ilgili operasyonlar içerir CRUD İşlemleri
+    {
+        //encapsulation
+        public void Add(Product product)
+        {
+            Console.WriteLine(product.ProductName + " EKlendi.");
+        }
+
+        public void Update(Product product)
+        {
+            Console.WriteLine(product.ProductName + " Güncellendi.");
+        }
+
+    }
+}
